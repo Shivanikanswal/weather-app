20a751fb8b6863aac88c191ee8676356a00b069f
Shivani Kanswal <shivanikanswal@gmail.com>
2024-05-05 19:29:20 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { ReactDOM } from "react";
+const App = () => {
+  return <div></div>;
+};
+
+const rootApp = ReactDOM.createRoot(document.getElementById("root"));
+
+rootApp.render(App);
