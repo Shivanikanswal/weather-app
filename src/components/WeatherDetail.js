d36246cff0bacc01197ef1699b2eef2aea328845
Shivani Kanswal <shivanikanswal@gmail.com>
2024-07-18 15:47:05 +0530

    script update and ui changes

--- src/components/WeatherDetail.js
+++ src/components/WeatherDetail.js
@@ -2,11 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import weatherImg from "../utils/images/all-weather.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCalendar,
-  faLocation,
-  faLocationPin,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCalendar, faLocationPin } from "@fortawesome/free-solid-svg-icons";
+import { LineChart } from "@mui/x-charts";
 // import { faSun } from "@fortawesome/free-solid-svg-icons";
 
 const WeatherDetail = (props) => {
@@ -34,6 +31,7 @@ const WeatherDetail = (props) => {
 
   const temp = weatherDetail?.main?.temp;
   const feels_like = weatherDetail?.main?.feels_like;
+  const humidity = weatherDetail?.main?.humidity;
   const weatherDescription = weatherDetail?.weather?.[0]?.description;
   const iconCode = weatherDetail?.weather?.[0]?.icon;
   // const { temp, feels_like } = weatherDetail?.main;
@@ -41,6 +39,8 @@ const WeatherDetail = (props) => {
   const event = new Date(oldTime * 1000);
   const newDate = event.toDateString();
   const newTime = event.toLocaleTimeString("en-US");
+  const windSpeed = weatherDetail?.wind?.speed;
+  const visibility = weatherDetail?.visibility;
 
   return (
     <div>
@@ -79,7 +79,7 @@ const WeatherDetail = (props) => {
                   {weatherDetail?.name}, {weatherDetail?.sys?.country}
                 </p>
               </div>
-              <div className="cal flex mb-3">
+              <div className="cal flex">
                 <p className=" pr-2">
                   <FontAwesomeIcon icon={faCalendar} />
                 </p>
@@ -90,7 +90,39 @@ const WeatherDetail = (props) => {
             </div>
           </div>
         </div>
-        <div className="others w-[75%] h-auto rounded-2xl ml-6 bg-[#05459c80] text-white backdrop-blur-sm"></div>
+        <div className="others w-[75%] h-auto rounded-2xl ml-6 bg-[#05459c80] text-white backdrop-blur-sm p-4">
+          <p className="m-3">Today's Highlight</p>
+          <div className="new-section flex">
+            <div className=" m-2 w-1/3 grid grid-rows-[160px_50px]">
+              <div className="wind bg-[#11407e80] rounded-md mb-4">
+                <p>Wind Status</p>
+                <span className="text-3xl">{windSpeed}km/h</span>
+              </div>
+              <div className="humidity bg-[#11407e80] rounded-md">
+                <p>Humidity</p>
+                <span className="text-3xl">{humidity}%</span>
+              </div>
+            </div>
+            <div className="m-2 w-1/3 grid grid-rows-[160px_50px]">
+              <div className="uvindex bg-[#11407e80] rounded-md mb-4">
+                UV Index
+              </div>
+              <div className="visibility bg-[rgba(17,64,126,0.5)] rounded-md">
+                <p>Visibility</p>
+                <span className="text-3xl">{visibility}</span>
+              </div>
+            </div>
+            <div className=" m-2 w-1/3 grid grid-rows-[160px_50px]">
+              <div className="sunrise-sunset bg-[#11407e80] rounded-md mb-4 ">
+                Sunrise Sunset
+              </div>
+              <div className="feels-like bg-[#11407e80] rounded-md">
+                <p>Feels like</p>
+                <span className="text-3xl">{feels_like}</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
